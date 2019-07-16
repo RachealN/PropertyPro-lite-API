f4f85a2c583b491bc21df5b88eff0cb227e747fe
Racheal <rechealmatsikoIT@gmail.com>
2019-07-16 22:46:57 +0300

    ch(database):setting up database[Finishes #167313324]

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-import routes from './src/routes/routes';
-import express from 'express';
-import bodyParser from 'body-parser'
 import { Server } from 'tls';
+import express from 'express';
+import bodyParser from 'body-parser';
+import Pool from './v2/models/db';
 
-const app = express()
+
+import routes from './src/routes/routes';
+
+const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use('/',routes)
+app.use('/', routes);
 app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to PropertyPro lite app' }));
 
 
