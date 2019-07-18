7ded553b2de6471cd20e322b2af9236c877e7965
Racheal <rechealmatsikoIT@gmail.com>
2019-07-18 07:48:29 +0300

    ft(login user):User/agent is able to login succesfully #167358798

--- v2/schema/schema.js
+++ v2/schema/schema.js
@@ -2,25 +2,25 @@ import bcrypt from 'bcryptjs';
 import pool from '../models/db';
 
 
-const Userschema = (values) => {
+const Userschema = async (values) => {
   const query = 'INSERT INTO users(firstName,lastName, address, email, password, isAdmin) VALUES($1,$2,$3,$4,$5,$6) RETURNING *';
-  try { return pool.query(query, values); } catch (error) {
+  try { return await pool.query(query, values); } catch (error) {
     return (0);
   }
 };
 
-const loginschema = (loginValues) => {
-  const loginQuery = `SELECT * FROM users WHERE email  = '${email}' and password= '${password}'` ;
-  try { return pool.query(loginQuery); } catch (error) {
+const loginSchema = async(loginValues) => {
+  const loginQuery = `SELECT * FROM users WHERE email  = '${loginValues.email}' and password= '${loginValues.password}'` ;
+  try { return await pool.query(loginQuery); } catch (error) {
     console.log(error)
     return (0);
   }
 };
 
 
-const checkEmail = (data) => {
+const checkEmail = async(data) => {
   const queryEmail = `SELECT * FROM users WHERE email ='${data}'`;
-  try { return pool.query(queryEmail); } catch (error) {
+  try { return await pool.query(queryEmail); } catch (error) {
     console.log(error)
     return (0);
   }
